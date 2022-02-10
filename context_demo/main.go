340a2f35c3b936425d8171ca149a2374755dc8c0
tonyxu2028 <liwei2028@163.com>
2022-02-10 16:06:45 +0800

    book_final_code

--- context_demo/main.go
+++ context_demo/main.go
@@ -1,13 +1,7 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"time"
-)
-
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
+	//ctx, cancel := context.WithCancel(context.Background())
 	//ctx, cancelFunc := context.WithDeadline(context.Background(),time.Now().Add(10*time.Second))
 	//ctx.Done()
 	//defer cancelFunc()
